421d917330dcf98ffc92073aa26a060fa3887381
Mohamed SAHIBI <msahib@open-25.CS.McGill.CA>
2012-09-28 16:32:19 -0400

    pretty printing

--- tiny/flex+bison/pretty.c
+++ tiny/flex+bison/pretty.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include "pretty.h"
 
-void prettyEXP(EXP *e)
+
+typedef enum {false = 0, true = 1 } bool;
+
+bool isLowerPrecedence(EXP *, EXP *);
+int calculatePrecedence(EXP *);
+
+void prettyEXP(EXP *e, EXP *parent)
 {
 switch (e->kind) {
     case idK:
@@ -11,58 +17,125 @@ switch (e->kind) {
          printf("%i",e->val.intconstE);
          break;
     case timesK:
-         printf("(");
-         prettyEXP(e->val.timesE.left);
+         if(isLowerPrecedence(e, parent)) 
+         {
+            printf("(");
+         }
+         prettyEXP(e->val.timesE.left, e);
          printf("*");
-         prettyEXP(e->val.timesE.right);
-         printf(")");
+         prettyEXP(e->val.timesE.right, e);
+         if(isLowerPrecedence(e, parent)) 
+         {
+            printf(")");
+         }
          break;
     case divK:
-         printf("(");
-         prettyEXP(e->val.divE.left);
+         if(isLowerPrecedence(e, parent)) 
+         {
+            printf("(");
+         }
+         prettyEXP(e->val.divE.left, e);
          printf("/");
-         prettyEXP(e->val.divE.right);
-         printf(")");
+         prettyEXP(e->val.divE.right, e);
+         if(isLowerPrecedence(e, parent)) 
+         {
+            printf(")");
+         }
          break;
     case modK:
-         printf("(");
-         prettyEXP(e->val.modE.left);
+         if(isLowerPrecedence(e, parent)) 
+         {
+            printf("(");
+         }
+         prettyEXP(e->val.modE.left, e);
          printf("%%");
-         prettyEXP(e->val.modE.right);
-         printf(")");
+         prettyEXP(e->val.modE.right, e);
+         if(isLowerPrecedence(e, parent)) 
+         {
+            printf(")");
+         }
          break;
     case absK:
          printf("abs(");
-         prettyEXP(e->val.absE.right);
+         prettyEXP(e->val.absE.right, e);
          printf(")");
          break;
     case negK:
          printf("-");
-         prettyEXP(e->val.negE.right);
+         prettyEXP(e->val.negE.right, e);
          break;
     case exponK:
-         printf("(");
-         prettyEXP(e->val.exponE.left);
+         if(isLowerPrecedence(e, parent)) 
+         {
+            printf("(");
+         }
+         prettyEXP(e->val.exponE.left, e);
          printf("**");
-         prettyEXP(e->val.exponE.right);
-         printf(")");
+         prettyEXP(e->val.exponE.right, e);
+         if(isLowerPrecedence(e, parent)) 
+         {
+            printf(")");
+         }
          break;
     case plusK:
-         printf("(");
-         prettyEXP(e->val.plusE.left);
+         if(isLowerPrecedence(e, parent)) 
+         {
+            printf("(");
+         }
+         prettyEXP(e->val.plusE.left, e);
          printf("+");
-         prettyEXP(e->val.plusE.right);
-         printf(")");
+         prettyEXP(e->val.plusE.right, e);
+         if(isLowerPrecedence(e, parent)) 
+         {
+            printf(")");
+         }
          break;
     case minusK:
-         printf("(");
-         prettyEXP(e->val.minusE.left);
+         if(isLowerPrecedence(e, parent)) 
+         {
+            printf("(");
+         }
+         prettyEXP(e->val.minusE.left, e);
          printf("-");
-         prettyEXP(e->val.minusE.right);
-         printf(")");
+         prettyEXP(e->val.minusE.right, e);
+         if(isLowerPrecedence(e, parent)) 
+         {
+            printf(")");
+         }
          break;
     default:
          printf("Wrong kind");
          break;
   }
+}
+
+
+bool isLowerPrecedence(EXP *e1, EXP *e2)
+{
+    if(e2 == NULL)
+    {
+        return false;
+    }
+
+    return calculatePrecedence(e1) < calculatePrecedence(e2);
+}
+
+int calculatePrecedence(EXP *e)
+{
+    if(e->kind == minusK || e->kind == plusK)
+    {
+        return 1; 
+    }
+    else if(e->kind == divK || e->kind == timesK || e->kind == modK)
+    {
+        return 2; 
+    }
+    else if (e->kind == exponK)
+    {
+        return 3; 
+    }
+    else 
+    {
+        return 0;
+    }
 }
