9e59a3c3d1d050b52c9035fa427d1c3a9531a6e8
dbhage <dbhage@6669c6d1-5a39-0410-bbc6-daf4d3d1134a>
2012-10-01 03:54:36 +0000

    Merge branch 'master' of https://github.com/AmineS/WIG_Compiler_520 into HEAD
    git-svn-id: svn+ssh://svn.cs.mcgill.ca/2012/groups/group-h@13221 6669c6d1-5a39-0410-bbc6-daf4d3d1134a

--- tiny/sablecc-3/tiny/Evaluator.java
+++ tiny/sablecc-3/tiny/Evaluator.java
@@ -9,11 +9,20 @@ import java.util.*;
 public class Evaluator extends DepthFirstAdapter
 {
   /* (static) eval function */
-  public static int eval(Node ast)
+  public static Node eval(Node ast)
   {
     Evaluator e = new Evaluator();
+    
     ast.apply(e);
-    return e.getValue(ast);
+    
+    if(e.hasValue(ast))
+    {
+       return new TNumber(Integer.toString(e.getValue(ast)));
+    }
+    else
+    {
+       return ast;
+    }    
   }
     
   /* Hashtable, holding intermediate values for AST nodes */
@@ -21,8 +30,7 @@ public class Evaluator extends DepthFirstAdapter
 
   /* Utility methods to set/get values for AST nodes */
   private void setValue(Node node, int value)
-  { values.put(node, new Integer(value));
-  }
+  { values.put(node, new Integer(value));}
 
   private int getValue(Node node)
   { /* gets and removes the associated value.
@@ -35,6 +43,9 @@ public class Evaluator extends DepthFirstAdapter
     return value.intValue();
   }
 
+  /* checks if the node has a value in the hash table */ 
+  private boolean hasValue(Node node)
+  { return null != values.get(node); }
   /* We deal with each grammar alternative, one by one */
 
   /* AST root (hidden [start = exp;] production) */
@@ -43,15 +54,58 @@ public class Evaluator extends DepthFirstAdapter
 
   /* plus */ 
   public void outAPlusExp(APlusExp node)
-  { setValue(node, getValue(node.getL()) + getValue(node.getR())); }
+  { 
+     if (getValue(node.getL())==0)
+     {
+        setValue(node, getValue(node.getR())); 
+     }
+     else if (getValue(node.getR())==0)
+     {
+        setValue(node, getValue(node.getL())); 
+     }
+     else
+     {
+        setValue(node, getValue(node.getL()) + getValue(node.getR())); 
+     }
+  }
   
   /* minus */
   public void outAMinusExp(AMinusExp node)
-  { setValue(node, getValue(node.getL()) - getValue(node.getR())); }
+  {
+     if (getValue(node.getL())==0)
+     {
+        setValue(node, -getValue(node.getR())); 
+     }
+     else if (getValue(node.getR())==0)
+     {
+        setValue(node, getValue(node.getL())); 
+     }
+     else
+     {
+        setValue(node, getValue(node.getL()) - getValue(node.getR())); 
+     }
+  }
 
   /* mult */
   public void outAMultExp(AMultExp node)
-  { setValue(node, getValue(node.getL()) * getValue(node.getR())); }
+  { 
+     if (getValue(node.getL()) == 0 || getValue(node.getR()) == 0)
+     {
+        setValue(node, 0); 
+     }
+     else if (getValue(node.getL())==1)
+     {
+        setValue(node, getValue(node.getR())); 
+     }
+     else if (getValue(node.getL())==1)
+     {
+        setValue(node, getValue(node.getR())); 
+     }
+     else
+     {
+        setValue(node, getValue(node.getL()) * getValue(node.getR())); 
+     }
+  }
 
   /* div */
   public void outADivdExp(ADivdExp node)
@@ -61,30 +115,90 @@ public class Evaluator extends DepthFirstAdapter
 		  System.out.println("Attempting division by zero!");
 		  System.exit(0);
 	  }	 	 
-	  setValue(node, getValue(node.getL()) / getValue(node.getR())); 
+	  else if (getValue(node.getL())==0)
+	  {
+        setValue(node, 0); 
+	  }
+	  else if (getValue(node.getR())==1)
+	  {
+        setValue(node, getValue(node.getL())); 
+	  }
+	  else
+	  {
+	     setValue(node, getValue(node.getL()) / getValue(node.getR())); 
+	  }
   }
   
   /* mod */
   public void outAModExp(AModExp node)
-  { setValue(node, getValue(node.getL()) % getValue(node.getR())); }
+  { 
+     if (getValue(node.getR()) == 0)
+     {
+        System.out.println("Attempting division by zero!");
+        System.exit(0);
+     }
+     else if (getValue(node.getL()) == 0)
+     {
+        setValue(node, 0);
+     }
+     else
+     {
+        setValue(node, getValue(node.getL()) % getValue(node.getR())); 
+     }
+  }
   
   /* exponentiation */
   public void outAExponExp(AExponExp node)
-  { setValue(node, (int)(Math.pow(getValue(node.getL()), getValue(node.getR())))); }
+  { 
+     if (getValue(node.getL())==0)
+     {
+        setValue(node, 0);
+     }
+     else if (getValue(node.getR())==0)
+     {
+        setValue(node, 1);
+     }
+     else if (getValue(node.getL())==1)
+     {
+        setValue(node, 1);
+     }
+     else if (getValue(node.getR())==1)
+     {
+        setValue(node, getValue(node.getL()));
+     }
+     else
+     {
+        setValue(node, (int)(Math.pow(getValue(node.getL()), getValue(node.getR()))));
+     }
+  }
   
   /* abs*/
   public void outAAbsExp(AAbsExp node)
   { setValue(node, Math.abs(getValue(node.getR()))); }
 
-  /* neg*/
+  /* neg */
   public void outANegExp(ANegExp node)
   { setValue(node, -1*(getValue(node.getR()))); }
   
   /* identifier (!!!) */
   public void outAIdExp(AIdExp node)
-  { throw new RuntimeException("I can't evaluate the value of an identifier!"); }
+  { return; }
   
   /* number */
   public void outANumberExp(ANumberExp node)
   { setValue(node, Integer.parseInt(node.getNumber().getText())); }
+  
+  private void replaceNodesWithValues(Node node)
+  {
+     //null check 
+     if(node == null) return; 
+     
+     //check if there is a value for the node 
+     Integer nodeValue = (Integer) this.values.get(node);     
+     if(nodeValue != null)
+     {
+        //if there is an integer value then return the value 
+        node.replaceBy(new TNumber(nodeValue.toString()));
+     }
+  }
 }
